c591205c880033f1dbd89953f52c6a8b92771e1b
dhakalsamundra <seapush02@gmail.com>
2020-09-30 19:05:14 +0300

    create component

---
+++ src/components/layout/SearchBar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const SearchBar = () => {
+const[search, setSearch] = useState('')
+  const onChange = e => {
+    setSearch(e.current.value);
+  };
+
+  return (
+    <nav style={{ marginBottom: '30px' }} className='blue'>
+      <div className='nav-wrapper'>
+        <form>
+          <div className='input-field'>
+            <input
+              id='search'
+              type='search'
+              placeholder='Search Logs..'
+              value={search}
+              onChange={onChange}
+            />
+            <label className='label-icon' htmlFor='search'>
+              <i className='material-icons'>search</i>
+            </label>
+            <i className='material-icons'>close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+SearchBar.propTypes = {
+  searchLogs: PropTypes.func.isRequired
+};
+
+export default SearchBar
