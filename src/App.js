52b0caf7cad99a8caa1227fe83952d2c610fae45
dhakalsamundra <seapush02@gmail.com>
2020-09-30 19:06:50 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Fragment, useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+import SearchBar from './components/layout/SearchBar'
+
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+      <Fragment>
+        <SearchBar />
+        
+      </Fragment>
+  );
+};
+
+export default App;
