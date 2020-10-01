d36f8a4e9eac2a4826eb5dfc7132e7e6190c4730
dhakalsamundra <seapush02@gmail.com>
2020-10-01 17:40:17 +0300

    combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,8 @@
 import { combineReducers } from 'redux';
+import logReducer from './log';
+import techReducer from './tech';
 
-export default combineReducers({});
+export default combineReducers({
+  log: logReducer,
+  tech: techReducer
+});
