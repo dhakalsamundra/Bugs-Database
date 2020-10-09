7c20c2bf72edd6fd16d4627693e46bfef00f399e
dhakalsamundra <seapush02@gmail.com>
2020-10-09 20:32:15 +0300

    add logs to ux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -19,6 +24,8 @@ const App = () => {
         <SearchBar />
         <div className='container'>
           <Logs />
+          <AddLogModal />
+          <EditLogModal />
         </div>
       </Fragment>
     </Provider>
